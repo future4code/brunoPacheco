09fe19c442323e7ce62161fd2f707c0a2873ac92
Bruno Pacheco <bruno_spacheco@yahoo.com>
2019-09-30 15:47:16 -0300

    adicionei o css

--- semana4/aula1/futuretube/src/App.js
+++ semana4/aula1/futuretube/src/App.js
@@ -10,7 +10,7 @@ function App() {
             <h1>FutureTube</h1>
         </div>
         <div className="buscar">
-           <input name="search" type="text" placeholder="buscar">
+           <input name="search" type="text" placeholder="buscar"></input>
         </div>
       </header>
       <div className="container">
@@ -20,6 +20,7 @@ function App() {
             <li><a href="">Em alta</a></li>
             <li><a href="">Inscrições</a></li>
             <li><a href="">Originais</a></li>
+            <hr></hr>
             <li><a href="">Biblioteca</a></li>
             <li><a href="">Histórico</a></li>
           </ul>
@@ -27,49 +28,49 @@ function App() {
         <section>
             <a href="media1.html">
               <div className="media">
-                <img src="/media/video1.png" alt="Media 1">
+                <img src="https://picsum.photos/id/856/160/180?v=sau" alt="Media 1"></img>
                 <p>Media 1</p>
               </div>
             </a>
             <a href="media2.html" >
               <div className="media">
-                <img src="/media/video2.png" alt="Media 2">
+                <img src="https://picsum.photos/id/856/160/180?v=sauds" alt="Media 2"></img>
                 <p>Media 2</p>
               </div>
             </a>
             <a href="media3.html">
               <div className="media">
-                <img src="/media/video3.png" alt="Media 3">
+                <img src="https://picsum.photos/id/856/160/180?v=sauymun" alt="Media 3"></img>
                 <p>Media 3</p>
               </div>
             </a>
             <a href="media4.html" >
               <div className="media">
-                <img src="/media/video4.png" alt="Media 4">
+                <img src="https://picsum.photos/id/856/160/180?v=sauyhtrre" alt="Media 4"></img>
                 <p>Media 4</p>
               </div>
             </a>
             <a href="media5.html" >
               <div className="media">
-                <img src="/media/video5.png" alt="Media 5">
+                <img src="https://picsum.photos/id/856/160/180?v=sauferfw" alt="Media 5"></img>
                 <p>Media 5</p>
               </div>
             </a>
             <a href="media6.html" >
               <div className="media">
-                <img src="/media/video6.png" alt="Media 6">
+                <img src="https://picsum.photos/id/856/160/180?v=saudewdx" alt="Media 6"></img>
                 <p>Media 6</p>
               </div>
             </a>
             <a href="media7.html" >
               <div className="media">
-                <img src="/media/video7.png" alt="Media 7">
+                <img src="https://picsum.photos/id/856/160/180?v=saufrew" alt="Media 7"></img>
                 <p>Media 7</p>
               </div>
             </a>
             <a href="media8.html" >
               <div className="media">
-                <img src="/media/video8.png" alt="Media 8">
+                <img src="https://picsum.photos/id/856/160/180?v=sauwwwqq" alt="Media 8"></img>
                 <p>Media 8</p>
               </div>
             </a>
