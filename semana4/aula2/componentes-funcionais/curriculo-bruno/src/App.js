cedc7097e13bc4766b6402706464f4ad68bfb210
Bruno Pacheco <bruno_spacheco@yahoo.com>
2019-10-01 15:50:53 -0300

    iniciando o projeto e realizando o primeiro commit

---
+++ semana4/aula2/componentes-funcionais/curriculo-bruno/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { BigCards } from './componentes/BigCard/BigCard';
+
+function App() {
+  return (
+    <div className="App">
+      <BigCards />
+    </div>
+  );
+}
+
+export default App;
---
+++ semana4/aula2/componentes-funcionais/curriculo-bruno/src/componentes/BigCard/BigCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './BigCard.css';
+
+export function BigCard() {
+    return (
+        <section>
+            <div className="logo">
+                <img src="" alt=""/>
+            </div>
+            <div className="apresentacao">
+                <p>Olá,eu sou o Bruno! Sou aluno da Future4</p>
+            </div>
+        </section>
+    )
+}
