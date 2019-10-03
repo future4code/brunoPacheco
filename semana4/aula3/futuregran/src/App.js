e2a9853a85134321c610ac0fa64dc61a17455c91
Bruno Pacheco <bruno_spacheco@yahoo.com>
2019-10-03 15:04:52 -0300

    começado a mexer com o arq css ainda da aula de comps-classes

--- semana4/aula3/futuregran/src/App.js
+++ semana4/aula3/futuregran/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
-
+import Autor from './componentes/Autor/Autor';
+import Foto from './componentes/Foto/Foto';
+import CurtirEComentar from './componentes/CurtirEComentar/CurtirEComentar';
 
 function App() {
   return (
     <div className="App">
-    
+      <section>
+        <Autor />
+        <hr/>
+        <Foto />
+        <hr/>
+        <CurtirEComentar />
+      </section>
     </div>
   );
 }
--- semana4/aula3/futuregran/src/componentes/autor/Autor.js
+++ semana4/aula3/futuregran/src/componentes/autor/Autor.js
@@ -1,3 +1,19 @@
-import React from 'react';
-import './Autor.css';
+import React from './node_modules/react'
+import "./Autor.css"
+import PropTypes from './node_modules/prop-types'
 
+function Autor(props) {
+    return (
+        <div>
+            <img id="imgAutor" src={props.imagem} alt="" />
+            <h3> {props.titulo}</h3>
+        </div>
+    )
+}
+
+Autor.propTypes = {
+    imagem: PropTypes.string.isRequired,
+    titulo: PropTypes.string.isRequired
+}
+
+export default Autor;
