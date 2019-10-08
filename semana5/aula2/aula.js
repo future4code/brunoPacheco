33001265806a6461ce4605af839c004d89d05189
Bruno Pacheco <bruno_spacheco@yahoo.com>
2019-10-08 15:03:50 -0300

    iniciando aula2

---
+++ semana5/aula2/aula.js
@@ -0,0 +1,37 @@
+function receberDoisNumeros (a,b) {
+let maior =a
+let menor =b
+if(a>b){
+    maior=a
+    menor=b
+} 
+
+
+    console.log(
+`
+Informações:
+    -O maior número é:${maior}
+    -a diferença entre eles é: ${maior-menor}
+
+`
+}
+
+
+//ex5
+
+class Cliente {
+    constructor(nome, genero){
+        this.nome = nome
+        this.genero=genero
+    }
+}
+
+ehFeminino = () =>{
+    
+}
+`
+"Olá,<nome>
+    Seja {nome.pessoa}} a nossa plataforma.
+        Estamos muito felizes em poder <>.
+        Aguarde contato para mais informações."`
+    `
