27d12203de6c81e30cca687c1b20f83ee2c52d49
Bruno Pacheco <bruno_spacheco@yahoo.com>
2019-12-02 15:00:45 -0300

    aula1-node.js

---
+++ semana11/aula1/gestorDeTarefas/gestorDeTarefas.js
@@ -0,0 +1,20 @@
+/*
+Crie uma aplicação Node que receba uma string representando o nome 
+da lista de tarefas e uma string representando uma nova tarefa, em seguida 
+o programa deve adicionar a nova tarefa em um arquivo que tenha o nome da lista 
+de tarefas.
+*/
+
+const fs = require('fs');
+const fileName = "tarefas.txt";
+const novaTarefa = process.argv[3];
+
+try{
+    const data = fs.readFileSync(fileName);
+    const dataTraduzida = data.toString();
+    const novaLista = dataTraduzida + `\n ${novaTarefa}`;
+    fs.appendFileSync(fileName,novaTarefa, 'utf8');
+    console.log("Tarefa adicionada com sucesso!");
+}catch(err){
+    console.error(err)
+}
