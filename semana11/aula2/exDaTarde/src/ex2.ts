137810a14b01c590dcc6dc5b24697f8fceb31197
Bruno Pacheco <bruno_spacheco@yahoo.com>
2019-12-03 17:52:16 -0300

    Ex3

--- semana11/aula2/exDaTarde/src/ex2.ts
+++ semana11/aula2/exDaTarde/src/ex2.ts
@@ -9,85 +9,38 @@
 */
 
 type post = {
-    text: string,
-    autor: string,
+  text: string,
+  autor: string,
 }
 
 const arrayDePosts: post[] = [
-    {
-        text: "Ouvir rock ativa o Coldplay que pode ativar o Los Hermanos",
-        autor: "Presidente da Funarte",
-    },
-    {
-        text: "Deus acime de todos, menos do preço da carne",
-        autor: "Biruliro",
-    },
-    {
-        text: "Trump vai taxar aço brasileiro e eu continuo tomando ferro",
-        autor: "Biruliro",
-    },
-    {
-        text: "Flamengo está a cinco minutos sem ganhar título",
-        autor: "Sofredor do Flamengo",
-    },
-    {
-        text: "QUeríamos o troféu de vôlei mas não foi possível!",
-        autor: "Sofredor do Flamengo",
-    },
+  {
+    text: "Ouvir rock ativa o Coldplay que pode ativar o Los Hermanos",
+    autor: "Presidente da Funarte",
+  },
+  {
+    text: "Deus acime de todos, menos do preço da carne",
+    autor: "Biruliro",
+  },
+  {
+    text: "Trump vai taxar aço brasileiro e eu continuo tomando ferro",
+    autor: "Biruliro",
+  },
+  {
+    text: "Flamengo está a cinco minutos sem ganhar título",
+    autor: "Sofredor do Flamengo",
+  },
+  {
+    text: "QUeríamos o troféu de vôlei mas não foi possível!",
+    autor: "Sofredor do Flamengo",
+  },
 ]
 
 function filtroDeAutor(arrayDePosts: post[], autor: string): post[] {
-    return arrayDePosts.filter(el: post)=> {
-        return el: post === autor
-    };
+  return arrayDePosts.filter((el) => {
+    return (el.autor === autor)
+  })
+}
 
+console.log(filtroDeAutor(arrayDePosts, "Biruliro"))
 
-    console.log(filtroDeAutor( arrayDePosts, "Sofredor do Flamengo" ))
-
-//Ex2
-
-    enum Marca {
-        FIAT = "fiat",
-        VOLKSWAGEN = "volkswagen"
-      }
-      ​
-      type carro = {
-        marca: Marca;
-        capacidadeDeGasolina: number;
-        flex: boolean;
-        litrosGastosParaAndar?: (km: number) => number;
-      };
-      ​
-      const uno: carro = {
-        marca: Marca.FIAT,
-        capacidadeDeGasolina: 60,
-        flex: true,
-        litrosGastosParaAndar: (quilometro: number): number => {
-          return 0.25 * quilometro;
-        }
-      };
-      ​
-      const fusca: carro = {
-        marca: Marca.VOLKSWAGEN,
-        capacidadeDeGasolina: 40,
-        flex: false
-      };
-      ​
-      const gol: carro = {
-        marca: Marca.VOLKSWAGEN,
-        capacidadeDeGasolina: 80,
-        flex: true
-      };
-      ​
-      const arrayDeCarros: carro[] = [uno, fusca, gol];
-      ​
-      // EXERCICIO 2
-      ​
-      function pegaCarrosPorMarca(carros: carro[], marca?: string): carro[] {
-        if (!marca) {
-          return carros.filter((el: carro) => {
-            return el.marca === marca;
-          });
-        }
-        return carros;
-      }
