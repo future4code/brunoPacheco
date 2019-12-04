6c67b6322e285049a2c526ad9cb6ac0dcf1f55d7
Bruno Pacheco <bruno_spacheco@yahoo.com>
2019-12-04 16:37:04 -0300

    exercício1 - diretório encontrado

--- semana11/aula3/src/ex1.ts
+++ semana11/aula3/src/ex1.ts
@@ -1,12 +1,19 @@
+
 import axios, { AxiosResponse } from 'axios';
 
-const fs = require('fs');
-const fileName1 = require("1.txt")
-const fileName2 = require("2.txt")
-const fileName3 = require("3.txt")
-const fileName4 = require("4.txt")
-const fileName5 = require("5.txt")
+const fs = require("fs")
 
-//fs.readdir(fileName1, function(files))
+const directoryPath = ('./src/textos')
 
-console.log(fileName1)
+const promise = new Promise((resolve, reject)=> {
+    fs.readdir(directoryPath, function (err: string, path: string[]) {
+
+    if(err) {
+        reject(err)
+        return
+    }
+   
+    
+})
+
+
