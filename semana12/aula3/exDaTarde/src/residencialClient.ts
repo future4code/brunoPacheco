e5c012230e668174e89c72fc64769c61f39ad290
Bruno Pacheco <bruno_spacheco@yahoo.com>
2019-12-11 15:29:57 -0300

    item 2

---
+++ semana12/aula3/exDaTarde/src/residencialClient.ts
@@ -0,0 +1,10 @@
+import { Residence } from "./residence";
+import { Client } from "./client";
+
+export class ResidencialClient extends Residence implements Client {
+    constructor(){
+        super()
+    }
+
+    
+}
