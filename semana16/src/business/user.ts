fa5f13a01b5daf7715c175222a2916422e01a2c2
xecoo <bruno_spacheco@yahoo.com>
2020-01-06 15:07:58 -0300

    iniciando o projeto pelas entidades

---
+++ semana16/src/business/user.ts
@@ -0,0 +1,9 @@
+export class User {
+    constructor(
+    private id: string,
+    private name: string,
+    private nickname: string,
+    private email: string,
+    private orderDate: Date){
+    }
+}
